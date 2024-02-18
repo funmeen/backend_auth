e2e5afd8d05dcb06b45019feffdde9bc02b728ac
funmeen <irfanmuhymeen@gmail.com>
2024-02-19 00:51:06 +0800

    update dotenv in server.js

--- server.js
+++ server.js
@@ -1,34 +1,32 @@
+require('dotenv').config();
 const express = require('express');
-const https = require('https');
-const fs = require('fs');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const https = require('https');
+const fs = require('fs');
 
 const app = express();
 
-// Load SSL certificate and private key
-const privateKey = fs.readFileSync('/etc/ssl/private.key', 'utf8');
-const certificate = fs.readFileSync('/etc/ssl/certificate.crt', 'utf8');
-const credentials = { key: privateKey, cert: certificate };
-
-// MongoDB connection
 mongoose.connect(process.env.MONGODB_URI)
-    .then(() => console.log('MongoDB Connected'))
-    .catch((err) => console.log(err));
+        .then(() => console.log('MongoDB Connected'))
+        .catch((err) => console.log(err));
 
-// Middleware
 app.use(express.json());
+
 app.use(cors({
     origin: 'https://testauthorization.netlify.app',
     credentials: true
-}));
+}))
 
-// Routes
 const userRouter = require('./routes/auth');
 app.use('/api/user', userRouter);
 
-// Start HTTPS server
+const privateKey = fs.readFileSync('/path/to/private.key', 'utf8');
+const certificate = fs.readFileSync('/path/to/certificate.crt', 'utf8');
+const credentials = { key: privateKey, cert: certificate };
+
 const httpsServer = https.createServer(credentials, app);
-httpsServer.listen(4000, () => {
-    console.log('Server is running on port 4000');
+
+httpsServer.listen(process.env.PORT || 443, () => {
+    console.log(`Server is running on port ${process.env.PORT || 443}`);
 });
