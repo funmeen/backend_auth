a3ab5559d06e15c4af8a097d2bedcc83b529070a
funmeen <irfanmuhymeen@gmail.com>
2024-02-27 10:22:17 +0800

    simple testing https

--- server.js
+++ server.js
@@ -1,35 +1,14 @@
-require('dotenv').config();
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
 const https = require('https');
 const fs = require('fs');
-const path = require('path');
 
-const app = express();
+const options = {
+    key: fs.readFileSync('/etc/ssl/private.key'),
+    cert: fs.readFileSync('/etc/ssl/certificate.crt')
+};
 
-// Load SSL/TLS certificates
-const privateKey = fs.readFileSync(path.resolve(__dirname, 'private.key'), 'utf8');
-const certificate = fs.readFileSync(path.resolve(__dirname, 'certificate.crt'), 'utf8');
-const credentials = { key: privateKey, cert: certificate };
-
-const httpsServer = https.createServer(credentials, app);
-
-mongoose.connect(process.env.MONGODB_URI)
-        .then(() => console.log('MongoDB Connected'))
-        .catch((err) => console.log(err));
-
-app.use(express.json());
-
-app.use(cors({
-    origin: 'http://localhost:3000',
-    credentials: true
-}))
-
-const userRouter = require('./routes/auth');
-app.use('/', userRouter);
-
-// Start the HTTPS server
-httpsServer.listen(process.env.PORT, () => {
-    console.log(`Server is running on port ${process.env.PORT}`);
+const server = https.createServer(options, (req, res) => {
+    res.writeHead(200);
+    res.end('Hello, HTTPS World!');
 });
+
+server.listen(443);
