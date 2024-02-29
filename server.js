d3e701799fb5b33c918b94eb7f967f32705eb47c
funmeen <irfanmuhymeen@gmail.com>
2024-02-29 22:06:21 +0800

    update location text file ii

--- server.js
+++ server.js
@@ -25,4 +25,5 @@ app.listen(process.env.PORT, () => {
 
 app.get('/.well-known/pki-validation/6C10D3FAEA175C2D5F286451A67B947E.txt', (req, res) => {
     res.sendFile('/home/ec2-user/backend_auth/6C10D3FAEA175C2D5F286451A67B947E.txt')
-})
+});
+
