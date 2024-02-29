7f107ddae3a9833a23b7b2b69c17b30ce0af5452
funmeen <irfanmuhymeen@gmail.com>
2024-02-29 22:09:14 +0800

    changing path from server to auth

--- server.js
+++ server.js
@@ -22,8 +22,3 @@ app.use('/', userRouter);
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`);
 });
-
-app.get('/.well-known/pki-validation/6C10D3FAEA175C2D5F286451A67B947E.txt', (req, res) => {
-    res.sendFile('/home/ec2-user/backend_auth/6C10D3FAEA175C2D5F286451A67B947E.txt')
-});
-
