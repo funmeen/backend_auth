a57310b3daacf34f953287b73a0a1ca25ec9ae45
funmeen <irfanmuhymeen@gmail.com>
2024-02-29 22:54:03 +0800

    try zerossl

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const fs = require('fs');
+
+const file = fs.readFileSync('/17BAD38A48CDD7A3E164DDA9C91C2198.txt');
 
 const app = express();
 
@@ -15,6 +18,10 @@ app.use(cors())
 
 const userRouter = require('./routes/auth');
 app.use('/', userRouter);
+
+app.get('/.well-known/pki-validation/17BAD38A48CDD7A3E164DDA9C91C2198.txt',(req, res) => {
+    res.sendFile(file)
+});
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`);
