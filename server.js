b3c88b61762a2eb4734c229bfafd4f84e3f38d89
funmeen <irfanmuhymeen@gmail.com>
2024-02-29 22:33:47 +0800

    revert coding

--- server.js
+++ server.js
@@ -2,12 +2,9 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const fs = require('fs');
-
-const file = fs.readFileSync('./6C10D3FAEA175C2D5F286451A67B947E.txt');
 
 const app = express();
-const port = 4000
+
 mongoose.connect(process.env.MONGODB_URI)
         .then(() => console.log('MongoDB Connected'))
         .catch((err) => console.log(err));
@@ -16,13 +13,11 @@ app.use(express.json());
 
 app.use(cors())
 
-app.get('/.well-known/pki-validation/6C10D3FAEA175C2D5F286451A67B947E.txt', (req, res) => {
-    res.sendFile('/home/ec2-user/backend_auth/6C10D3FAEA175C2D5F286451A67B947E.txt')
-});
-
 const userRouter = require('./routes/auth');
 app.use('/', userRouter);
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
 });
+
+
